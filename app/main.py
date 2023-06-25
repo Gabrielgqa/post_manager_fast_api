1e84875a3523402a6c2f2437d20346cfddd1ddd7
GabrielGQA <gabrielgqa@gmail.com>
2023-06-24 22:45:31 -0300

    feat: import routes

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from . import models
-from .database import engine, get_db
+from .database import engine
+from .routers import post, user
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.include_router(post.router)
+app.include_router(user.router)
 
 @app.get("/")
 async def root():
